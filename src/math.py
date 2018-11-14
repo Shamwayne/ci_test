04334b056b77208fc0efc02a38ed30be38fffa94
Shingai Shamu <mshamu95@gmail.com>
2018-11-14 15:00:30 +0200

    Add files via upload

---
+++ src/math.py
@@ -0,0 +1,20 @@
+# Maths functions test
+
+class Math:
+	""" class of demo maths function """
+
+	def addition(val1, val2):
+		""" add 2 numbers together """
+		return val1 + val2
+
+	def subtraction(val1, val2):
+		""" gets the difference between 2 numbers """
+		return val1 - val2
+
+	def multiplication(val1, val2):
+		""" gets the product of 2 numbers """
+		return val1 * val2
+
+	def division(val1, val2):
+		""" gets the quotient of 2 numbers """
+		return val1 / val2
---
+++ tests/test-math.py
@@ -0,0 +1,17 @@
+import unittest
+from src.math import Math
+
+class MathTest(unittest.TestCase):
+
+	def test_addition(self):
+		# Make test fail
+		self.assertEqual(Math.addition(3,4), 8)
+
+	def test_subtraction(self):
+		self.assertEqual(Math.subtraction(5,2), 3)
+
+	def test_multiplication(self):
+		self.assertEqual(Math.multiplication(3,5), 15)
+
+	def test_division(self):
+		self.assertEqual(Math.division(4,2), 2)
